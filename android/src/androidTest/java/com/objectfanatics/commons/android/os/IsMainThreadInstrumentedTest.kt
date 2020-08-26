64a14c1fa9c30136853758ac3b423478bba678b0
beyondseeker <makoto.sato@objectfanatics.com>
2020-08-27 06:12:05 +0900

    rename
    - rename package.kt to isMainThread.kt
    - rename PackageKtInstrumentedTest.kt to IsMainThreadInstrumentedTest.kt

--- android/src/androidTest/java/com/objectfanatics/commons/android/os/IsMainThreadInstrumentedTest.kt
+++ android/src/androidTest/java/com/objectfanatics/commons/android/os/IsMainThreadInstrumentedTest.kt
@@ -8,7 +8,7 @@ import org.hamcrest.MatcherAssert
 import org.junit.Assert
 import org.junit.Test
 
-class PackageKtInstrumentedTest {
+class IsMainThreadInstrumentedTest {
     /**
      * Running on worker thread.
      */
